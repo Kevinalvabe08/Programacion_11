14122227ae59b1b1106a17831d595f29e2181f46
Kevinalvabe08 <Kevinalvaradob20@gmail.com>
2025-02-10 22:47:34 -0600

    tarea001pipu
    Signed-off-by: Kevinalvabe08 <Kevinalvaradob20@gmail.com>

---
+++ tarea001/tarea01.cpp
@@ -0,0 +1,19 @@
+intmain() {
+    //Variables
+    float numero1, numero2;
+    // se pide al usuario 2 numeros
+    cout << "Intruduce el primer numero deseado"
+    cin >> numero1;
+    cout << "Introduce el segundo numero deseado"
+    cin >> numero2;
+
+    //calcular la suma y muktiplicacion de los numeros dados previamente
+    float suma = numero1 + numero2;
+    float producto = numero1 * numero2;
+
+    //Ensena los productos dados
+    cout << "Su suma es de:" << suma << end1;
+    cout << "Su multiplicacion es de:" << producto << end;
+    
+    return 0;
+}
